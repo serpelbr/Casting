d57129f19e7d266e0ef8ba63d36e33db7c40befa
serpelbr <serpelbr@gmail.com>
2018-09-20 13:03:47 -0300

    Initial commit

---
+++ src/Casting.java
@@ -0,0 +1,33 @@
+public class Casting {
+    public static void main(String[] args){
+        double d = 3.1415;
+        System.out.println("Double d: " + d);
+        int    i = (int) d;
+        System.out.println("Int i: "  + i);
+        int i2 = (int) 3.1415;
+
+        double d2 = 5;
+        System.out.println("Double d2: "+d2);
+        int i3 = (int) d2;
+        System.out.println("Int i3: " + i3);
+
+        int d4 = 10;
+        double d3 = d4;
+
+        long l = 1000000000;
+        System.out.println("LONG l: "+l);
+        int i4 = (int) l;
+        System.out.println("Int i4: "+i4);
+
+        float f = 3.1415f;
+
+
+
+
+
+
+
+
+
+    }
+}
